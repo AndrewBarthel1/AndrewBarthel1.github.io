c510868aebc8483a26160c21aadc4e1c299d3b2b
AndrewBarthel <barthela2@gmail.com>
2025-09-21 22:34:09 -0600

    community

---
+++ week5/comcode.js
@@ -0,0 +1,21 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+var menushow = false;
+
+function showMenu() {
+    var shown = navMenu.classList.toggle("show");
+    navMenu.classList.toggle("hide");
+
+    if(shown){
+        navToggle.setAttribute("aria-expanded", "true");
+    }
+
+    else {
+        navToggle.setAttribute("aria-expanded", "false");
+    }
+
+}
+
+navToggle.addEventListener('click', showMenu);
+
