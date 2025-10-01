cd7ad8d09cac57fb454e5581f93aae2f7d92d5ed
AndrewBarthel <barthela2@gmail.com>
2025-09-30 22:53:52 -0600

    week6done

--- week6/comcode.js
+++ week6/comcode.js
@@ -28,8 +28,6 @@ trailLinks.forEach(span => {
     const url = event.currentTarget.dataset.href
 
 
-alert("hello");
-
     if(url)
     {
         window.location.href = url;
@@ -40,22 +38,103 @@ alert("hello");
 
 
 
-const filterButton = document.querySelector('.trailhead-filter');
-const filterItems = document.querySelector('.trailhead-filter-list')
+const filterButton = document.querySelectorAll('.trailhead-filter');
+const filterText = document.querySelectorAll('.trailhead-filter-text');
+const grid = document.querySelector('.grid-system')
+const filterList = document.querySelector('.trailhead-filter-list')
+const changeFilter = document.querySelectorAll('.filter-item')
+const trailheads = document.querySelectorAll('.trailhead-containers')
 
-// Add click event to each button
+
 filterButton.forEach(nav => {
-  nav.addEventListener('hover', (event) => {
+  nav.addEventListener('mouseenter', (event) => {
 
-    alert("hovering")
-filterItems.forEach(ul => {
-    ul.style.display = 'block'
-  })
+    filterList.classList.remove('hidden');
+
+//    document.querySelector('.trailhead-filter-list').classList.remove('hidden');
+    //const filterValue = event.target.textContent.toLowerCase();
+   // filterPhotos(filterValue);
+  });
+});
+
+filterButton.forEach(nav => {
+  nav.addEventListener('mouseleave', (event) => {
+
+    filterList.classList.add('hidden');
 
     //const filterValue = event.target.textContent.toLowerCase();
    // filterPhotos(filterValue);
   });
 });
+
+changeFilter.forEach(li => {
+  li.addEventListener('click', (event) => {
+    filterText[0].textContent = 'Filter: ' + li.textContent;
+    for(let i = 0; i < trailheads.length; i++)
+    {
+        if(trailheads[i].getAttribute('data-catagory') == li.textContent || li.textContent == "None")
+        {
+            trailheads[i].classList.remove('hidden')
+        }
+        else{
+            trailheads[i].classList.add('hidden')
+        }
+    }
+
+    //const filterValue = event.target.textContent.toLowerCase();
+   // filterPhotos(filterValue);
+  });
+});
+
+const eventZoom = document.querySelectorAll('.event');
+const eventInfo = document.querySelectorAll('.before');
+const eventFull = document.querySelectorAll('.after')
+
+eventZoom.forEach(div => {
+  div.addEventListener('click', (event) => {
+
+    if(div.classList.contains('blowup'))
+            {
+                eventReset()
+            }
+            else{
+    for(let i = 0; i < eventZoom.length; i++)
+    {
+
+        if(eventZoom[i].getAttribute('data-cell') == div.getAttribute('data-cell'))
+        {
+            eventZoom[i].classList.remove('hidden')
+            eventZoom[i].classList.add('blowup')
+            eventInfo[i].classList.add('hidden')
+            eventFull[i].classList.remove('hidden')
+        }
+        else{
+            eventZoom[i].classList.add('hidden')
+            eventZoom[i].classList.remove('blowup')
+            eventInfo[i].classList.remove('hidden')
+            eventFull[i].classList.add('hidden')
+        }
+    }
+    }
+
+    //const filterValue = event.target.textContent.toLowerCase();
+   // filterPhotos(filterValue);
+  });
+});
+
+function eventReset(){
+        for(let i = 0; i < eventZoom.length; i++)
+        {
+            eventZoom[i].classList.remove('hidden')
+            eventZoom[i].classList.remove('blowup')
+            eventInfo[i].classList.remove('hidden')
+            eventFull[i].classList.add('hidden')
+        }
+}
+
+
+
+
 
 /*function filterPhotos(category) {
   trailLinks.forEach(card => {
